90cd770b80cb2c3324b6c1de6a311c287b1a554b
E-I-M-C <mormons.at@gmail.com>
2024-09-25 18:11:43 -0700

    changed a .js value and some more css

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,14 +1,17 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
+const hr = document.querySelector("hr");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("🕶️")) {
-        main.style.background = "#000";
+        main.style.background = "#111";
         main.style.color = "#fff";
         modeButton.textContent = "🔆";
+        hr.style.borderTop = "1px solid #fff";
     } else {
-        main.style.background = "#eee";
+        main.style.background = "#fff";
         main.style.color = "#000";
         modeButton.textContent = "🕶️";
+        hr.style.borderTop = "1px solid #000";
     }
 });
