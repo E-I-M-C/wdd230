e4aab768c0255621364885708b21d26a93cb9458
E-I-M-C <mormons.at@gmail.com>
2024-10-10 16:13:22 -0700

    Made more changes to course form page

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,4 @@
+// Displays input range value
 const rangevalue = document.querySelector("#rate-num");
 const range = document.querySelector("#rate");
 
@@ -6,4 +7,20 @@ range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
+}
+
+// Displays text when passwords don't match
+const password = document.querySelector("#pass");
+const confirmPassword = document.querySelector("#con-pass");
+const message = document.querySelector("#message");
+
+confirmPassword.addEventListener("focusout", onChange);
+
+function onChange() {
+    if (password.value != confirmPassword.value) {
+        message.textContent = "Not Matching";
+        message.style.color = "red";
+    } else {
+        message.textContent = "";
+    }
 }
