c6d850e7d8de0c2a7c54f66d51919b3f873149c5
E-I-M-C <mormons.at@gmail.com>
2024-10-24 12:26:05 -0700

    Added tempurature forecast

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -19,7 +19,23 @@ async function apiFetchWeather() {
     }
 }
 
+async function apiFetchForecast() {
+    try {
+        const response = await fetch(urltwo);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data);
+            displayForecastResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 apiFetchWeather();
+apiFetchForecast();
 
 function displayWeatherResults(data) {
     const weatherIcon = document.createElement('img');
@@ -27,7 +43,7 @@ function displayWeatherResults(data) {
     const currentTemp = document.createElement('span');
     const captionDesc = document.createElement('p');
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
+    const desc = data.weather[0].description;
 
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
@@ -57,5 +73,31 @@ function calculateWindChill(temperature, windSpeed, unit) {
         return 0.0817(3.71 * windSpeed ** 0.5 + 5.81 - 0.25 * windSpeed)(temperature - 91.4) + 91.4;
     } else if (unit == 'celsius') {
         return 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
+    }
+}
+
+function displayForecastResults(data) {
+    const forecastGrid = document.querySelector('.weather-grid');
+
+    for (let i = 6; i < 30; i += 8) {
+        const forecast = data.list[i];
+        const forecastDay = document.createElement('div');
+        const title = document.createElement('h3');
+        const icon = document.createElement('img');
+        const temperature = document.createElement('span');
+        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        const dayDate = new Date(forecast.dt_txt);
+        const iconsrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+        const desc = forecast.weather[0].description;
+
+        title.textContent = `${days[dayDate.getDay()]} ${dayDate.getDate()}`;
+        icon.setAttribute('src', iconsrc);
+        icon.setAttribute('alt', desc);
+        temperature.innerHTML = `${forecast.main.temp}&deg;F`;
+
+        forecastDay.appendChild(title);
+        forecastDay.appendChild(icon);
+        forecastDay.appendChild(temperature);
+        forecastGrid.appendChild(forecastDay);
     }
 }
